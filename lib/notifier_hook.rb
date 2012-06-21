6782f7845327032bae217a6380b75a0ea3d2e603
xdite <xdite@rocodev.com>
2012-06-21 10:35:21 +0800

    use shout-bot to replace irccat

--- lib/notifier_hook.rb
+++ lib/notifier_hook.rb
@@ -1,5 +1,6 @@
 # -*- encoding : utf-8 -*-
-require 'actionpack'
+require 'rubygems'
+require 'shout-bot'
 class NotifierHook < Redmine::Hook::Listener
 
   def controller_issues_new_after_save(context = { })
@@ -7,9 +8,11 @@ class NotifierHook < Redmine::Hook::Listener
     @issue = context[:issue]
     @author = @issue.author
     @assigned_message = @issue.assigned_to.nil? ? "無" : "#{@issue.assigned_to.lastname} #{@issue.assigned_to.firstname}"
-    speak "#{@author.lastname} #{@author.firstname} 建立主旨:「#{@issue.subject}」"
-    speak "狀態:「#{@issue.status.name}」. 分派給:「#{@assigned_message}」. 意見:「#{truncate_words(@issue.description)}」"
-    speak "網址: http://#{Setting.host_name}/issues/#{@issue.id}"
+    messages = []
+    messages << "#{@author.lastname} #{@author.firstname} 建立主旨:「#{@issue.subject}」"
+    messages << "狀態:「#{@issue.status.name}」. 分派給:「#{@assigned_message}」. 意見:「#{truncate_words(@issue.description)}」"
+    messages << "網址: http://#{Setting.host_name}/issues/#{@issue.id}"
+    speak messages
   end
 
   def controller_issues_edit_before_save(context = { })
@@ -19,39 +22,57 @@ class NotifierHook < Redmine::Hook::Listener
     @editor = @journal.user
     @assigned_message = issue_assigned_changed?(@issue)
     @status_message = issue_status_changed?(@issue)
-    speak "#{@editor.lastname} #{@editor.firstname} 編輯主旨:「#{@issue.subject}」"
-    speak "狀態:「#{@status_message}」. 分派給:「#{@assigned_message}」. 意見:「#{truncate_words(@journal.notes)}」"
-    speak "網址: http://#{Setting.host_name}/issues/#{@issue.id}"
+    messages = []
+    messages << "#{@editor.lastname} #{@editor.firstname} 編輯主旨:「#{@issue.subject}」"
+    messages << "狀態:「#{@status_message}」. 分派給:「#{@assigned_message}」. 意見:「#{truncate_words(@journal.notes)}」"
+    messages << "網址: http://#{Setting.host_name}/issues/#{@issue.id}"
+    speak messages
   end
 
   def controller_messages_new_after_save(context = { })
     @project = context[:project]
     @message = context[:message]
     @author = @message.author
-    speak "#{@author.lastname} #{@author.firstname} wrote a new message「#{@message.subject}」on #{@project.name}:「#{truncate_words(@message.content)}」"
-    speak "網址: http://#{Setting.host_name}/boards/#{@message.board.id}/topics/#{@message.root.id}#message-#{@message.id}"
+    messages = []
+    messages << "#{@author.lastname} #{@author.firstname} wrote a new message「#{@message.subject}」on #{@project.name}:「#{truncate_words(@message.content)}」"
+    messages << "網址: http://#{Setting.host_name}/boards/#{@message.board.id}/topics/#{@message.root.id}#message-#{@message.id}"
+    speak messages
   end
 
   def controller_messages_reply_after_save(context = { })
     @project = context[:project]
     @message = context[:message]
     @author = @message.author
-    speak "#{@author.lastname} #{@author.firstname} replied a message「#{@message.subject}」on #{@project.name}: 「#{truncate_words(@message.content)}」"
-    speak "網址: http://#{Setting.host_name}/boards/#{@message.board.id}/topics/#{@message.root.id}#message-#{@message.id}"
+    messages = []
+    messages << "#{@author.lastname} #{@author.firstname} replied a message「#{@message.subject}」on #{@project.name}: 「#{truncate_words(@message.content)}」"
+    messages << "網址: http://#{Setting.host_name}/boards/#{@message.board.id}/topics/#{@message.root.id}#message-#{@message.id}"
+    speak messages
   end
 
   def controller_wiki_edit_after_save(context = { })
     @project = context[:project]
     @page = context[:page]
     @author = @page.content.author
-    speak "#{@author.lastname} #{@author.firstname} edited the wiki「#{@page.pretty_title}」on #{@project.name}."
-    speak "網址: http://#{Setting.host_name}/projects/#{@project.identifier}/wiki/#{@page.title}"
+    messages = []
+    messages << "#{@author.lastname} #{@author.firstname} edited the wiki「#{@page.pretty_title}」on #{@project.name}."
+    messages << "網址: http://#{Setting.host_name}/projects/#{@project.identifier}/wiki/#{@page.title}"
+    speak messages
   end
 
 private
 
-  def speak(message)
-    system("echo -n '#{message}' | nc localhost 5678")
+  def speak(messages)
+    login_name = "name"
+    login_password = "password"
+    chat_room = "#your-chatroom"
+    job = fork do
+      ShoutBot.shout("irc://#{login_name}:#{login_password}@irc.freenode.net:6667/#{chatroom}") do |channel|
+        messages.each do |m|
+          channel.say m
+        end
+      end
+    end
+    Process.detach(job)
   end
 
   def truncate_words(text, length = 20, end_string = '…')
